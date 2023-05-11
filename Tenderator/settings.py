cc91f0be8be70e098eb2ddc8c3856f112b5184ae
Denis Glushakov <denis.glushakov@bk.ru>
2023-05-11 17:46:51 +0300

    change form of STATIC_DIR

--- Tenderator/settings.py
+++ Tenderator/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -124,7 +125,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "Tenderator/static", ]
+# STATICFILES_DIRS = [BASE_DIR / "Tenderator/static", ]
+STATIC_DIR = os.path.join(BASE_DIR, 'Tenderator/static')
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
