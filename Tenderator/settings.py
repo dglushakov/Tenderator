2af6a5f3baff606b2004460b36ebaf60efcdf655
Denis Glushakov <denis.glushakov@bk.ru>
2023-05-11 18:20:08 +0300

    STATIC_ROOT changed to BASE_DIR/static

--- Tenderator/settings.py
+++ Tenderator/settings.py
@@ -124,7 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = '/static/'
+# STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_DIRS = [BASE_DIR / "Tenderator/static", ]
 STATIC_DIR = os.path.join(BASE_DIR, 'Tenderator/static')
 STATICFILES_DIRS = [STATIC_DIR, ]
