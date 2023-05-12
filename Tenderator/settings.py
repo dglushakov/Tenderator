d6fcb1ad10491e95190238abeaae21388f529c8a
Denis Glushakov <denis.glushakov@bk.ru>
2023-05-12 10:58:27 +0300

    settings

--- Tenderator/settings.py
+++ Tenderator/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = "/var/www/Tenderator/static/"
 # STATICFILES_DIRS = [BASE_DIR / "Tenderator/static", ]
 STATIC_DIR = os.path.join(BASE_DIR, 'Tenderator', 'static')
-STATICFILES_DIRS = [STATIC_DIR, ]
+STATICFILES_DIRS = [STATIC_DIR, STATIC_ROOT]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
